30884a025f5c5fa93edb4e7f2a84b4050d08d454
fivewincow <fivewincow@gmail.com>
2023-04-12 23:04:45 +0900

    first commit

---
+++ src/main/java/com/fivewincow/healthmarketback/HealthMarketBackApplication.java
@@ -0,0 +1,13 @@
+package com.fivewincow.healthmarketback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthMarketBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthMarketBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fivewincow/healthmarketback/HealthMarketBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivewincow.healthmarketback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMarketBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
